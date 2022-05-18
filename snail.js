47c5df850c3473a37576cbcd9336cdc7bbffd41d
Mihail OLARU <mihai_olaru@ymail.com>
2022-05-18 17:25:39 +0300

    Snail on going. Try separate vars for directions.

--- snail.js
+++ snail.js
@@ -1,19 +1,42 @@
 const snail = [
-    [1,  2,  3,  4,  5],
-    [16, 17, 18, 19, 6],
-    [15, 24, 25, 20, 7],
-    [14, 23, 22, 21, 8],
-    [13, 12, 11, 10, 9],
+  [1, 2, 3, 4, 5],
+  [16, 17, 18, 19, 6],
+  [15, 24, 25, 20, 7],
+  [14, 23, 22, 21, 8],
+  [13, 12, 11, 10, 9],
 ];
 
-function parseSnail( arr ) {
-	for( let i = 0; i < arr.length; i++ ){
-		for( let d = 0; d < i; d++ ){
-			for( let y = 0; y < arr.length; y++ ){
-				console.log(arr[i][y]);
-			}
-		}
-	}
+function parseSnail(arr) {
+  let count = arr.length * arr[0].length;
+
+  let row = 0;
+  let left = 0;
+  let right = 0;
+  let down = 0;
+
+  while (count > 0) {
+    while (left < arr[row].length) {
+      console.log(
+        `State: count: ${count}, row: ${row}, left: ${left}, right: ${right}, down: ${down}`
+      );
+
+      console.log(arr[row][left]);
+      left++;
+      count--;
+      down = arr.length;
+    }
+
+    row++;
+
+    while (down > 0) {
+      console.log(
+        `State: count: ${count}, row: ${row}, left: ${left}, right: ${right}, down: ${down}`
+      );
+
+      console.log(arr[down - 1][row]);
+      down--;
+    }
+  }
 }
 
 parseSnail(snail); // => Output
@@ -43,4 +66,4 @@ parseSnail(snail); // => Output
 // 23
 // 24
 // 25
-// 
+//
