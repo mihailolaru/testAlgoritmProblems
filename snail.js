79a379a8ccd46af15f37e6b3f2350043448537df
Mihail OLARU <mihai_olaru@ymail.com>
2022-05-19 23:28:11 +0300

    code ooptimization

--- snail.js
+++ snail.js
@@ -6,69 +6,7 @@ const snail = [
   [13, 12, 11, 10, 9],
 ];
 
-const snail2 = [
-  [1, 2, 3],
-  [8, 9, 4],
-  [7, 6, 5],
-];
-
-const snail3 = [
-  [1, 2, 3],
-  [12, 13, 4],
-  [11, 14, 5],
-  [10, 15, 6],
-  [9, 8, 7],
-];
-
-function parseSnail(arr) {
-  let arrLength = arr.length;
-  let increment = 1;
-  let startPoint = 0; 
-
-  // right
-  for (let x = 0; x < arrLength - increment; x++) {
-    console.log(`right [${startPoint}][${x}] `, arr[startPoint][x]);   
-  }
-
-  //down
-  for (let x = 0; x < arrLength - increment; x++) {
-    console.log(
-      `down [${x}][${arrLength - increment}] `,
-      arr[x][arrLength - 1]
-    );  
-  }
-
-  //left
-  for (let x = arrLength - increment; x > 0; x--) {
-    console.log(
-      `left [${arrLength - increment}][${x}] `,
-      arr[arrLength - increment][x]
-    );  
-  }
-
-  //up
-  for (let x = arrLength - increment; x > 0; x--) {
-    console.log(`up [${x}][${startPoint}] `, arr[x][startPoint]); 
-  }
-
-  arrLength--;
-  increment++;
-  startPoint++;
-}
-
-parseSnail(snail2);
-
 /*
-0 [0, 1, 2]
-1 [0, 1, 2]
-2 [0, 1, 2]
-
-right 0.0 0.1 
-down  0.2 1.2
-left  2.2 2.1
-up    2.0 1.0
-right 1.1
-
 0 [ 0, 1, 2, 3, 4],
 1 [ 0, 1, 2, 3, 4],
 2 [ 0, 1, 2, 3, 4],
@@ -84,5 +22,69 @@ down  1.3 2.3
 left  3.3 3.2
 up    3.1 
 right 2.1 2.2
-
 */
+
+const snail2 = [
+  [1, 2, 3],
+  [8, 9, 4],
+  [7, 6, 5],
+];
+
+/*
+0 [0, 1, 2]
+1 [0, 1, 2]
+2 [0, 1, 2]
+
+right 0.0 0.1 
+down  0.2 1.2
+left  2.2 2.1
+up    2.0 1.0
+right 1.1
+*/
+
+const snail3 = [
+  [1, 2, 3],
+  [12, 13, 4],
+  [11, 14, 5],
+  [10, 15, 6],
+  [9, 8, 7],
+];
+
+function parseSnail(arr) {
+  let arrLength = arr.length;
+  let increment = 1;
+  let startPoint = 0;
+
+  // TODO Figure out the general loop
+  // right
+  for (let x = 0; x < arrLength - increment; x++) {
+    console.log(`right [${startPoint}][${x}] `, arr[startPoint][x]);
+  }
+
+  //down
+  for (let x = 0; x < arrLength - increment; x++) {
+    console.log(
+      `down  [${x}][${arrLength - increment}] `,
+      arr[x][arrLength - 1]
+    );
+  }
+
+  //left
+  for (let x = arrLength - increment; x > 0; x--) {
+    console.log(
+      `left  [${arrLength - increment}][${x}] `,
+      arr[arrLength - increment][x]
+    );
+  }
+
+  //up
+  for (let x = arrLength - increment; x > 0; x--) {
+    console.log(`up    [${x}][${startPoint}] `, arr[x][startPoint]);
+  }
+
+  arrLength--;
+  increment++;
+  startPoint++;
+}
+
+parseSnail(snail2);
